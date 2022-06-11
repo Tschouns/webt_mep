7d26d232b559e4fe488b71eae2cb40c3c44646fb
Jonas <jonas.aklin@gmail.com>
2022-06-11 14:57:15 +0200

    Improved PHP program structure.

--- htdocs/test.php
+++ htdocs/test.php
@@ -1,45 +1,3 @@
-<?php
-    $html_content = prepareHtmlContent();
-
-    function prepareHtmlContent()
-    {
-        [$id_entry_date, $id_mood_slider, $id_entry_text] = array("entry_date", "mood_slider", "entry_text");
-
-        if (!array_key_exists($id_entry_date, $_POST) || !validateDate($_POST[$id_entry_date]))
-        {
-            return "The parameter " . $id_entry_date . " is missing, or not a valid date.";
-        }
-
-        if (!array_key_exists($id_mood_slider, $_POST) ||
-            !is_numeric($_POST[$id_mood_slider]) ||
-            (intval($_POST[$id_mood_slider])) < 1 ||
-            (intval($_POST[$id_mood_slider])) > 10)
-        {
-           return "The parameter " . $id_mood_slider . " is missing, or not a valid number between 1 and 10.";
-        }
-
-        if (!array_key_exists($id_entry_text, $_POST) || strlen($_POST[$id_entry_text]) == 0)
-        {
-            return "The parameter " . $id_entry_text . " is missing, or empty.";
-        }
-
-        $entry_date = $_POST[$id_entry_date];
-        $mood_slider = $_POST[$id_mood_slider];
-        $entry_text = $_POST[$id_entry_text];
-
-        $text = $entry_date . " " . $mood_slider . " " . $entry_text;
-
-        return "<p>" . $text ."</p>";
-    }
-
-    function validateDate($date_string)
-    {
-        $format = 'Y-m-d';
-        $d = DateTime::createFromFormat($format, $date_string);
-
-        return $d && $d->format($format) === $date_string;
-    }
-?>
 <html>
     <head>
         <title>Tagebuch mit Stimmungsbarometer</title>
@@ -49,6 +7,50 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <?php echo $html_content ?>
+<?php
+
+function validateDate($date_string) {
+    $format = 'Y-m-d';
+    $d = DateTime::createFromFormat($format, $date_string);
+
+    return $d && $d->format($format) === $date_string;
+}
+
+function validateParameters() {
+    if (!array_key_exists("entry_date", $_POST) || !validateDate($_POST["entry_date"])) {
+        echo "<p>The parameter 'entry_date' is missing, or not a valid date.</p>";
+        return false;
+    }
+
+    if (!array_key_exists("mood_slider", $_POST) ||
+        !is_numeric($_POST["mood_slider"]) ||
+        (intval($_POST["mood_slider"])) < 1 ||
+        (intval($_POST["mood_slider"])) > 10) {
+       echo "<p>The parameter 'mood_slider' is missing, or not a valid number between 1 and 10.</p>";
+       return false;
+    }
+
+    if (!array_key_exists("entry_text", $_POST) || strlen($_POST["entry_text"]) == 0) {
+        echo "<p>The parameter 'entry_text' is missing, or empty.</p>";
+        return false;
+    }
+
+    return true;
+}
+
+/* Main */
+if (!validateParameters()) {
+    return;
+}
+
+$entry_date = $_POST["entry_date"];
+$mood_slider = $_POST["mood_slider"];
+$entry_text = $_POST["entry_text"];
+
+$text = $entry_date . " " . $mood_slider . " " . $entry_text;
+
+echo "<p>" . $text ."</p>";
+
+?>
     </body>
 </html>

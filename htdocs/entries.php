96c7137e2d696235d0c29ce37b535d3826d2d74c
Jonas <jonas.aklin@gmail.com>
2022-06-12 23:50:22 +0200

    Added W3.css.

--- htdocs/entries.php
+++ htdocs/entries.php
@@ -168,6 +168,7 @@ function prepareHtmlContent() {
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Gentium+Plus:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <link rel="stylesheet" href="css/style.css">
         <link rel="stylesheet" href="css/entries.css">
         <script type="text/javascript" src="js/graph.js"></script>

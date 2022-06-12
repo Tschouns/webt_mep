1ddee73dd5e0c9ab99346c6454b284145b097154
Jonas <jonas.aklin@gmail.com>
2022-06-12 22:38:24 +0200

    Added text validation.

--- htdocs/js/form.js
+++ htdocs/js/form.js
@@ -11,11 +11,27 @@ function validateEntryDate() {
     let date = new Date(datePicker.value);
     if (isNaN(date.getTime()) || date.getTime() !== date.getTime()) {
         datePickerMessage.innerHTML = "Das ausgew&auml;hlte Datum ist nicht g&uuml;ltig.";
-
         return false;
     }
 
-    datePickerMessage.innerHTML = date;
-
+    datePickerMessage.innerHTML = "";
     return true;
+}
+
+function validateEntryText() {
+    let textBox = document.getElementById("entry_text");
+    let textBoxMessage = document.getElementById("entry_text_message");
+
+    if (textBox.value == null || textBox.value.length == 0) {
+        textBoxMessage.innerHTML = "Es muss ein Text eingegeben werden.";
+        return false;
+    }
+
+    textBoxMessage.innerHTML = "";
+    return true;
+}
+
+function validateForm() {
+    validateEntryDate();
+    validateEntryText();
 }

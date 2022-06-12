29331881004a52169874c5c74bfade96bba5b35b
Jonas <jonas.aklin@gmail.com>
2022-06-13 00:31:08 +0200

    Style improvements.

--- htdocs/entries.php
+++ htdocs/entries.php
@@ -99,7 +99,7 @@ function displayEntries($conn) {
     echo "<script id=\"mood_data\" type=\"application/json\">$mood_data_json_array</script>";
     echo "<script>mood_data = JSON.parse(document.getElementById(\"mood_data\").text); </script>";
     echo "<canvas id=\"graph_canvas\" alt=\"Graphische Darstellung des Stimmungsverlaufs\"></canvas>";
-    echo "</article";
+    echo "</article>";
 
     // Entry table article.
     echo "<article>";

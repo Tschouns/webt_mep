c1c8cef15b03427acef7f360b52d1100ee98f4fd
Jonas <jonas.aklin@gmail.com>
2022-06-12 17:51:58 +0200

    Dates are property sorted.

--- htdocs/js/graph.js
+++ htdocs/js/graph.js
@@ -1,15 +1,29 @@
 function drawGraph(canvasId, data) {
-    alert(data[0]["entry_date"]);
-
     // Get the canvas and context.
-    var canvas = document.getElementById(canvasId);
-    var context = canvas.getContext("2d");
+    let canvas = document.getElementById(canvasId);
+    let context = canvas.getContext("2d");
 
     // Clear for (re)drawing.
     context.clearRect(0, 0, canvas.width, canvas.height);
 
+    // Check number of entries... need at least 2.
+    if (data.length < 2) {
+        context.font = "10px Arial";
+        context.fillText("Need at least two entries.", 10, 10);
+        return;
+    }
+
+    // Determine dates and step sizes.
+    let dataPoints = data.map(i => ({ date: new Date(i.entry_date), mood: i.mood }));
+    dataPoints.sort((a, b) => b.date - a.date);
+
+    let earliestDate = dataPoints[0].date;
+    let latestDate = dataPoints[dataPoints.length - 1].date;
+
     // Draw stuff.
     context.font = "30px Arial";
     context.fillText("Hello World", 10, 50);
+    context.fillText(earliestDate, 10, 70);
+    context.fillText(latestDate, 10, 90);
 }
 
